bc3a02f3c5609232c1393e29756672d6e046b7c2
student <abdullahihamdiabass@gmail.com>
2021-02-21 10:13:27 +0300

    :

---
+++ src/hello.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var name:String="sarah"
+    println(name)
+var age:Int=19
+    println(age)
+    var phoneNumber:String="0757444205"
+    println(phoneNumber)
+    var kenyan:Boolean=true
+    println(kenyan)
+    var weight:Float=52.5f
+    println(weight)
+}
+
